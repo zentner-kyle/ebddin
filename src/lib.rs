67f41e2157a138f29905c2a1e6d22d4ccc5730d6
Kyle Zentner <zentner.kyle@gmail.com>
2017-06-02 18:00:16 -0700

    Evaluate `Diagram`s

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,65 @@
+#![allow(dead_code)]
+extern crate bit_vec;
+extern crate smallvec;
+extern crate vec_map;
+
+use bit_vec::BitVec;
+
 mod diagram;
+
+use diagram::{Diagram, NodeX};
+
+fn evaluate_diagram(diagram: &Diagram, root: usize, input: &BitVec) -> bool {
+    let mut node = root;
+    loop {
+        match diagram.expand(node) {
+            NodeX::Leaf { value } => {
+                return value;
+            }
+            NodeX::Branch {
+                variable,
+                low,
+                high,
+            } => {
+                if input[variable] {
+                    node = high;
+                } else {
+                    node = low;
+                }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn evaluate_zero() {
+        let mut d = Diagram::new();
+        let zero = d.zero();
+        assert_eq!(evaluate_diagram(&d, zero, &BitVec::from_elem(0, false)),
+                   false);
+    }
+
+    #[test]
+    fn evaluate_one() {
+        let mut d = Diagram::new();
+        let one = d.one();
+        assert_eq!(evaluate_diagram(&d, one, &BitVec::from_elem(0, false)),
+                   true);
+    }
+
+    #[test]
+    fn evaluate_identity() {
+        let mut d = Diagram::new();
+        let zero = d.zero();
+        let one = d.one();
+        let branch = d.branch(0, zero, one);
+        assert_eq!(evaluate_diagram(&d, branch, &BitVec::from_elem(1, false)),
+                   false);
+        assert_eq!(evaluate_diagram(&d, branch, &BitVec::from_elem(1, true)),
+                   true);
     }
 }
